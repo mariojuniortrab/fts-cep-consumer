d2c3f04d89e87a97b7098fbe4598d0c9249729c1
Mario Junior <mariojuniortrab@gmail.com>
2024-07-08 16:05:40 -0300

    before showcase

--- fts-command-ms/src/main/java/com/ftscepconsumer/fts_command_ms/configs/RabbitMQConfig.java
+++ fts-command-ms/src/main/java/com/ftscepconsumer/fts_command_ms/configs/RabbitMQConfig.java
@@ -1,6 +1,5 @@
 package com.ftscepconsumer.fts_command_ms.configs;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
